23f3108550adacb74c226421a9c6d40e80ac2b35
mohitvashu1 <mohitchoubeybca@gmail.com>
2025-07-24 08:49:28 -0700

    Logout

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -2,8 +2,37 @@ import React from 'react'
 import NavBar from '../Components/NavBar.jsx'
 import { Outlet } from 'react-router-dom'
 import Footer from './Footer.jsx'
+import axios from 'axios'
+import { useDispatch } from 'react-redux'
+import {addUser} from '../utils/userSlice.js'
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux'
 
 const body = () => {
+ const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const userData = useSelector((store) => store.user);
+
+  const fetchUser= async () => {
+    if (userData) return;
+    try{
+      const res = await  axios.get("http://localhost:3001/profile/view",{
+        withCredentials: true
+      });
+      dispatch(addUser(res.data));
+    } catch (err) {
+      if (err.status === 401) {
+        navigate("/login");
+      }
+      console.error(err);
+    }
+  };
+  
+  useEffect(() => {
+    fetchUser();
+  }, []);
+
   return (
       <>
       <NavBar/>
--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,14 +1,45 @@
-import React from 'react'
+import axios from 'axios';
 import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { removeUser } from '../utils/userSlice';
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+
+
 const NavBar = () => {
 
   const user = useSelector((store) => store.user);
-  console.log(user)
+  const  dispatch = useDispatch();
+  const navigate = useNavigate();
+
+
+  const handleLogout = async() =>{
+    try{
+      const res = await axios.post(
+        "http://localhost:3001/logout",
+        {},
+        {
+          withCredentials: true,
+        }
+      );
+      dispatch(removeUser())
+      return navigate("/login");
+    }catch(err) {
+      console.error("Logout failed", err);
+    }
+  }
 
   return (
      <div className="navbar bg-base-200 shadow-sm">
   <div className="flex-1">
-    <a className="btn btn-ghost text-xl">DevTinder</a>
+    <Link to="/" className="btn btn-ghost normal-case text-xl">
+      <img
+        src="https://static.vecteezy.com/system/resources/previews/018/910/833/non_2x/tinder-app-logo-tinder-app-icon-free-free-vector.jpg"
+        alt="logo"
+        className="w-10 h-10 mr-2"
+      />
+      DevTINDER
+    </Link>
   </div>
   {user &&(
     <div className="flex gap-2">
@@ -25,13 +56,14 @@ const NavBar = () => {
         tabIndex={0}
         className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
         <li>
-          <a className="justify-between">
+          <Link to='/profile' className="justify-between">
             Profile
             <span className="badge">New</span>
-          </a>
+          </Link>
         </li>
         <li><a>Settings</a></li>
-        <li><a>Logout</a></li>
+        <li>
+          <a onClick={handleLogout}>Logout</a></li>
       </ul>
     </div>
   </div>
