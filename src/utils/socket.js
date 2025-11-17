a7667662d397170c324bb103cf0437bf7633a94a
mohitvashu1 <mohitchoubeybca@gmail.com>
2025-11-18 01:39:31 +0530

    update 2

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -3,8 +3,14 @@ import { BASE_URL } from "./constants";
 
 export const createSocketConnection = () => {
   if (location.hostname === "localhost") {
-    return io(BASE_URL);
+    return io("http://localhost:3001", {
+      withCredentials: true,
+      path: "/socket.io",
+    });
   } else {
-    return io("/", { path: "/api/socket.io" });
+    return io("http://13.232.171.142:3001", {
+      withCredentials: true,
+      path: "/api/socket.io",
+    });
   }
 };
